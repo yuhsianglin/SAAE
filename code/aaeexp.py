fa26e3586cf45f8a28d1e28382cbf667e79f90e2
Yu-Hsiang Lin <hitr2997925@gmail.com>
2017-12-04 23:26:57 -0500

    working...

--- code/aaeexp.py
+++ code/aaeexp.py
@@ -32,25 +32,145 @@ class aaeexp(object):
 		#self.graph = tf.Graph()
 
 
-	# Loss functions for joint training
-	def eval_entropy(self, X_tilde_logit, X):
-		#ave_entropy = tf.reduce_mean( tf.reduce_sum( tf.nn.sigmoid_cross_entropy_with_logits(labels = X, logits = X_tilde_logit), axis = 1 ) )
-		ave_entropy = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(labels = X, logits = X_tilde_logit) )
-		return ave_entropy
 
-	
-	def eval_gen_loss(self, ave_entropy, disc_res_neg, H, T):
-		# Note that tf.nn.l2_loss() has already included the 1/2 factor
-		# loss = ave_entropy + self.reg_lambda * ( tf.nn.l2_loss(W_e) )
-		#gen_loss = ave_entropy + tf.reduce_mean( tf.log( 1.0 - disc_res_neg ) ) + tf.reduce_mean(tf.reduce_sum(tf.pow(T - H, 2), axis = 1))
-		gen_loss = ave_entropy + tf.reduce_mean( tf.log( 1.0 - disc_res_neg ) ) + self.match_coef * tf.reduce_mean(tf.pow(T - H, 2))
-		return gen_loss
+	def train(self, W_e_file_name = None, b_e_file_name = None, b_d_file_name = None, W1_file_name = None, b1_file_name = None, W2_file_name = None, b2_file_name = None):
+		#with self.graph.as_default():
+		
+		sess = tf.Session()
+
+		if W_e_file_name == None:
+			rng_ae = 1.0 / math.sqrt( float( self.input_dim + self.hid_dim ) )
+			W_e = tf.Variable( tf.random_uniform( [self.input_dim, self.hid_dim], minval = -rng_ae, maxval = rng_ae ), name = "W_e" )
+			b_e = tf.Variable(tf.zeros([self.hid_dim]), name = "b_e")
+
+			b_d = tf.Variable(tf.zeros([self.input_dim]), name = "b_d")
+
+			rng1 = 1.0 / math.sqrt( float( self.hid_dim + self.d1 ) )
+			W1 = tf.Variable( tf.random_uniform( [self.hid_dim, self.d1], minval = -rng1, maxval = rng1 ), name = "W1" )
+			b1 = tf.Variable(tf.zeros([self.d1]), name = "b1")
+
+			rng2 = 1.0 / math.sqrt( float( self.d1 + 1 ) )
+			W2 = tf.Variable( tf.random_uniform( [self.d1, 1], minval = -rng2, maxval = rng2 ), name = "W2" )
+			b2 = tf.Variable(tf.zeros([1]), name = "b2")
+
+			X = tf.placeholder( tf.float32, [None, self.input_dim], name = "X" )
+			# Positive samples
+			Z = tf.placeholder( tf.float32, [None, self.hid_dim], name = "Z" )
+
+			H = tf.sigmoid( tf.matmul(X, W_e) + b_e )
+			X_tilde_logit = tf.matmul(H, tf.transpose(W_e)) + b_d
+
+			ave_entropy = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(labels = X, logits = X_tilde_logit) )
+
+			# Discriminate positive samples
+			Z1_pos = tf.sigmoid( tf.matmul(Z, W1) + b1 )
+			Z2_pos = tf.sigmoid( tf.matmul(Z1_pos, W2) + b2 )
+			disc_res_pos = Z2_pos
+
+			gen_loss = ave_entropy + tf.reduce_mean( tf.log( 1.0 - disc_res_neg ) ) + self.match_coef * tf.reduce_mean(tf.pow(T - H, 2))
+			disc_loss = -tf.reduce_mean( tf.log( disc_res_pos ) ) - tf.reduce_mean( tf.log( 1.0 - disc_res_neg ) )
+
+			tf.add_to_collection("gen_loss", gen_loss)
+			tf.add_to_collection("disc_loss", disc_loss)
+
+			#train_gen_step = tf.train.MomentumOptimizer(self.lrn_rate, self.momentum).minimize(gen_loss)
+			#train_disc_step = tf.train.MomentumOptimizer(self.lrn_rate, self.momentum).minimize(disc_loss)
+
+			train_gen_step = tf.train.AdagradOptimizer(self.lrn_rate).minimize(gen_loss)
+			train_disc_step = tf.train.AdagradOptimizer(self.lrn_rate).minimize(disc_loss)
+
+			tf.add_to_collection("train_gen_step", train_gen_step)
+			tf.add_to_collection("train_disc_step", train_disc_step)
+
+			# For test set
+			t = tf.placeholder(tf.float32, [self.hid_dim])
+			neg_dist_from_t = -tf.reduce_sum( tf.pow(H - t, 2), axis = 1 )
+			#T_test_full = self.attrdata.X[150:200]
+
+			sess.run( tf.global_variables_initializer() )
+
+			saver = tf.train.Saver()
+		else:
+			saver = tf.train.import_meta_graph(self.log_file_name_head + ".meta")
+			saver.restore(sess, tf.train.latest_checkpoint("./log/"))
+
+			graph = tf.get_default_graph()
+
+			W_e = graph.get_tensor_by_name("W_e")[0]
+			b_e = graph.get_tensor_by_name("b_e")[0]
+			b_d = graph.get_tensor_by_name("b_d")[0]
+			W1 = graph.get_tensor_by_name("W1")[0]
+			b1 = graph.get_tensor_by_name("b1")[0]
+			W2 = graph.get_tensor_by_name("W2")[0]
+			b2 = graph.get_tensor_by_name("b2")[0]
+
+			train_gen_step = tf.get_collection("train_gen_step")[0]
+			gen_loss = tf.get_collection("gen_loss")[0]
+
+			train_disc_step = tf.get_collection("train_disc_step")[0]
+			disc_loss = tf.get_collection("disc_loss")[0]
 
 
-	def eval_disc_loss(self, disc_res_pos, disc_res_neg):
-		# loss = ave_entropy + self.reg_lambda * ( tf.nn.l2_loss(W_e) )
-		disc_loss = -tf.reduce_mean( tf.log( disc_res_pos ) ) - tf.reduce_mean( tf.log( 1.0 - disc_res_neg ) )
-		return disc_loss
+			"""
+			W_e = tf.Variable(np.load(W_e_file_name), name = "W_e")
+			b_e = tf.Variable(np.load(b_e_file_name), name = "b_e")
+
+			b_d = tf.Variable(np.load(b_d_file_name), name = "b_d")
+
+			W1 = tf.Variable(np.load(W1_file_name), name = "W1")
+			b1 = tf.Variable(np.load(b1_file_name), name = "b1")
+
+			W2 = tf.Variable(np.load(W2_file_name), name = "W2")
+			b2 = tf.Variable(np.load(b2_file_name), name = "b2")
+			"""
+
+
+
+
+		log_file = open(self.log_file_name_head + '.txt', 'w+')
+		self.write_log(log_file, -1, 0.0, 0.0, X, Z, T, sess, gen_loss, disc_loss, t, neg_dist_from_t)
+		#self.write_log(log_file, -1, 0.0, X, Z, None, sess, gen_loss, disc_loss)
+
+		total_time_begin = time.time()
+		epoch = 0
+		while epoch < self.epoch_max:
+			time_begin = time.time()
+
+			self.data.initialize_batch('train')
+			self.gaus_sample.initialize_batch('train')
+			#self.attrdata.initialize_batch('train')
+			while self.data.has_next_batch():
+				X_batch, Y_batch, _, _, index_vector = self.data.next_batch()
+				Z_batch, _, _, _, _ = self.gaus_sample.next_batch()
+				#T_batch = self.attrdata.next_batch(index_vector)
+				T_batch = self.attrdata.next_batch("train", index_vector)
+				feed_dict = { X: X_batch, Z: Z_batch, T: T_batch }
+				#feed_dict = { X: X_batch, Z: Z_batch }
+				_, train_gen_loss_got = sess.run([train_gen_step, gen_loss], feed_dict = feed_dict)
+				_, train_disc_loss_got = sess.run([train_disc_step, disc_loss], feed_dict = feed_dict)
+			# End of all mini-batches in an epoch
+
+			time_end = time.time()
+			time_epoch = time_end - time_begin
+
+			total_time_end = time.time()
+			total_time = total_time_end - total_time_begin
+			
+			if (epoch + 1) % self.write_model_log_period == 0:
+				saver.save(sess, self.log_file_name_head)
+				#self.write_model_param(sess, W_e, b_e, b_d, W1, b1, W2, b2)
+				#self.write_H(X, H, sess)
+			
+			# Tried going through again the full training set to evaluate training loss
+			# Confirmed: Does not make difference
+			self.write_log(log_file, epoch, time_epoch, total_time, X, Z, T, sess, gen_loss, disc_loss, t, neg_dist_from_t, train_gen_loss_given = train_gen_loss_got, train_disc_loss_given = train_disc_loss_got)
+			#self.write_log(log_file, epoch, time_epoch, X, Z, None, sess, gen_loss, disc_loss, train_gen_loss_given = train_gen_loss_got, train_disc_loss_given = train_disc_loss_got)
+
+			epoch += 1
+		# End of all epochs
+		#total_time_end = time.time()
+		#total_time = total_time_end - total_time_begin
+		log_file.close()
 
 
 	# Write log
@@ -131,114 +251,4 @@ class aaeexp(object):
 		np.save(self.log_file_name_head + '_H.npy', H_got)
 	"""
 
-	def train(self, W_e_file_name = None, b_e_file_name = None, b_d_file_name = None, W1_file_name = None, b1_file_name = None, W2_file_name = None, b2_file_name = None):
-		#with self.graph.as_default():
-		X = tf.placeholder( tf.float32, [None, self.input_dim] )
-
-		if W_e_file_name == None:
-			rng_ae = 1.0 / math.sqrt( float( self.input_dim + self.hid_dim ) )
-			W_e = tf.Variable( tf.random_uniform( [self.input_dim, self.hid_dim], minval = -rng_ae, maxval = rng_ae ) )
-			b_e = tf.Variable(tf.zeros([self.hid_dim]))
-
-			b_d = tf.Variable(tf.zeros([self.input_dim]))
-
-			rng1 = 1.0 / math.sqrt( float( self.hid_dim + self.d1 ) )
-			W1 = tf.Variable( tf.random_uniform( [self.hid_dim, self.d1], minval = -rng1, maxval = rng1 ) )
-			b1 = tf.Variable(tf.zeros([self.d1]))
-
-			rng2 = 1.0 / math.sqrt( float( self.d1 + 1 ) )
-			W2 = tf.Variable( tf.random_uniform( [self.d1, 1], minval = -rng2, maxval = rng2 ) )
-			b2 = tf.Variable(tf.zeros([1]))
-		else:
-			W_e = tf.Variable(np.load(W_e_file_name))
-			b_e = tf.Variable(np.load(b_e_file_name))
-
-			b_d = tf.Variable(np.load(b_d_file_name))
-
-			W1 = tf.Variable(np.load(W1_file_name))
-			b1 = tf.Variable(np.load(b1_file_name))
-
-			W2 = tf.Variable(np.load(W2_file_name))
-			b2 = tf.Variable(np.load(b2_file_name))
-
-
-		H = tf.sigmoid( tf.matmul(X, W_e) + b_e )
-		X_tilde_logit = tf.matmul(H, tf.transpose(W_e)) + b_d
-		ave_entropy = self.eval_entropy(X_tilde_logit, X)
-
-		# Positive samples
-		Z = tf.placeholder( tf.float32, [None, self.hid_dim] )
-
-		# Discriminate positive samples
-		Z1_pos = tf.sigmoid( tf.matmul(Z, W1) + b1 )
-		Z2_pos = tf.sigmoid( tf.matmul(Z1_pos, W2) + b2 )
-		disc_res_pos = Z2_pos
-
-		# Text features in the shape of [batch_size, hid_dim]
-		T = tf.placeholder( tf.float32, [None, self.hid_dim] )
-
-		# Discriminate negative samples
-		Z1_neg = tf.sigmoid( tf.matmul(H, W1) + b1 )
-		Z2_neg = tf.sigmoid( tf.matmul(Z1_neg, W2) + b2 )
-		disc_res_neg = Z2_neg
-
-		gen_loss = self.eval_gen_loss(ave_entropy, disc_res_neg, H, T)
-		disc_loss = self.eval_disc_loss(disc_res_pos, disc_res_neg)
-
-		#train_gen_step = tf.train.MomentumOptimizer(self.lrn_rate, self.momentum).minimize(gen_loss)
-		#train_disc_step = tf.train.MomentumOptimizer(self.lrn_rate, self.momentum).minimize(disc_loss)
-
-		train_gen_step = tf.train.AdagradOptimizer(self.lrn_rate).minimize(gen_loss)
-		train_disc_step = tf.train.AdagradOptimizer(self.lrn_rate).minimize(disc_loss)
-
-		# For test set
-		t = tf.placeholder(tf.float32, [self.hid_dim])
-		neg_dist_from_t = -tf.reduce_sum( tf.pow(H - t, 2), axis = 1 )
-		#T_test_full = self.attrdata.X[150:200]
-
-		sess = tf.Session()
-		sess.run( tf.global_variables_initializer() )
-
-		log_file = open(self.log_file_name_head + '.txt', 'w+')
-		self.write_log(log_file, -1, 0.0, 0.0, X, Z, T, sess, gen_loss, disc_loss, t, neg_dist_from_t)
-		#self.write_log(log_file, -1, 0.0, X, Z, None, sess, gen_loss, disc_loss)
-
-		total_time_begin = time.time()
-		epoch = 0
-		while epoch < self.epoch_max:
-			time_begin = time.time()
-
-			self.data.initialize_batch('train')
-			self.gaus_sample.initialize_batch('train')
-			#self.attrdata.initialize_batch('train')
-			while self.data.has_next_batch():
-				X_batch, Y_batch, _, _, index_vector = self.data.next_batch()
-				Z_batch, _, _, _, _ = self.gaus_sample.next_batch()
-				#T_batch = self.attrdata.next_batch(index_vector)
-				T_batch = self.attrdata.next_batch("train", index_vector)
-				feed_dict = { X: X_batch, Z: Z_batch, T: T_batch }
-				#feed_dict = { X: X_batch, Z: Z_batch }
-				_, train_gen_loss_got = sess.run([train_gen_step, gen_loss], feed_dict = feed_dict)
-				_, train_disc_loss_got = sess.run([train_disc_step, disc_loss], feed_dict = feed_dict)
-			# End of all mini-batches in an epoch
-
-			time_end = time.time()
-			time_epoch = time_end - time_begin
-
-			total_time_end = time.time()
-			total_time = total_time_end - total_time_begin
-			
-			if (epoch + 1) % self.write_model_log_period == 0:
-				self.write_model_param(sess, W_e, b_e, b_d, W1, b1, W2, b2)
-				#self.write_H(X, H, sess)
-			
-			# Tried going through again the full training set to evaluate training loss
-			# Confirmed: Does not make difference
-			self.write_log(log_file, epoch, time_epoch, total_time, X, Z, T, sess, gen_loss, disc_loss, t, neg_dist_from_t, train_gen_loss_given = train_gen_loss_got, train_disc_loss_given = train_disc_loss_got)
-			#self.write_log(log_file, epoch, time_epoch, X, Z, None, sess, gen_loss, disc_loss, train_gen_loss_given = train_gen_loss_got, train_disc_loss_given = train_disc_loss_got)
-
-			epoch += 1
-		# End of all epochs
-		#total_time_end = time.time()
-		#total_time = total_time_end - total_time_begin
-		log_file.close()
+	
